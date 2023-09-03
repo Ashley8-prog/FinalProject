f3562618ea36a99aa5271248e67b987c5eb499c3
Kianny <kianny720@gmail.com>
2023-09-03 15:16:56 -0600

    cambios

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -6,12 +6,12 @@ export const Navbar = () => {
     <nav className="navbar navbar-light bg-light">
       <div className="container">
         <Link to="/">
-          <span className="navbar-brand mb-0 h1">React Boilerplatesss</span>
+          <span className="navbar-brand mb-0 h1">React Boilerplate</span>
         </Link>
         <div className="ml-auto">
           <Link to="/demo">
             <button className="btn btn-primary">
-              Check the Context in action
+              Check the context in action
             </button>
           </Link>
           <Link to="/signup">
